f46df6961ee1d09b40fd597b15c1002ed35567cb
Animesh Samantaray <animeshsamantaray2@gmail.com>
2026-07-14 12:16:56 +0530

    feat: Google Authentication backend completed

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -22,6 +22,11 @@ const userSchema = new mongoose.Schema(
       minlength: 6,
       select: false,
     },
+     googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
 
     avatar: {
       type: String,
