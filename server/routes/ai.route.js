c17a079a3ade344e77f64264f3d8b3d32a9fb047
Animesh Samantaray <animeshsamantaray2@gmail.com>
2026-07-30 18:54:54 +0530

    added protection layer  for the unauthenticated users

--- server/routes/ai.route.js
+++ server/routes/ai.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { askToAI } from "../controllers/ai.controller.js";
-
+import authMiddleware from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 
-router.post("/chat", askToAI);
+router.post("/chat",authMiddleware ,  askToAI);
 
 export default router;
